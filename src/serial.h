d459d995098a7aea793a8a1659f1704df06ea450
Kennedy, Matthew C <mk@gatech.edu>
2016-12-30 06:18:46 -0800

    Update serial header

--- src/serial.h
+++ src/serial.h
@@ -1,12 +1,14 @@
 #ifndef __SERIAL_H
 #define __SERIAL_H
 
+#include "stdint.h"
+
 void Init_Serial();
 
 
 
-
-
+void Serial_Send(const uint8_t* data, uint16_t length);
+void Serial_SendStr(const char* str);
 
 
 
